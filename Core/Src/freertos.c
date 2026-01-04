3ea8ec0519a7160a5bd4ed00cc46bbc50c9f3254
Blinest <1071378062@qq.com>
2026-01-04 21:23:49 +0800

    stm32f103c8t6 freertos init

---
+++ Core/Src/freertos.c
@@ -0,0 +1,179 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "Types/MotorCtrlType.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for VolSignTask */
+osThreadId_t VolSignTaskHandle;
+const osThreadAttr_t VolSignTask_attributes = {
+  .name = "VolSignTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for MotorCtrlTask */
+osThreadId_t MotorCtrlTaskHandle;
+const osThreadAttr_t MotorCtrlTask_attributes = {
+  .name = "MotorCtrlTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
+};
+/* Definitions for MotorDataTask */
+osThreadId_t MotorDataTaskHandle;
+const osThreadAttr_t MotorDataTask_attributes = {
+  .name = "MotorDataTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for VolDataTask */
+osThreadId_t VolDataTaskHandle;
+const osThreadAttr_t VolDataTask_attributes = {
+  .name = "VolDataTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for VolQueue */
+osMessageQueueId_t VolQueueHandle;
+const osMessageQueueAttr_t VolQueue_attributes = {
+  .name = "VolQueue"
+};
+/* Definitions for MotorQueue */
+osMessageQueueId_t MotorQueueHandle;
+const osMessageQueueAttr_t MotorQueue_attributes = {
+  .name = "MotorQueue"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartVolSignTask(void *argument);
+extern void startMotorCtrlTask(void *argument);
+extern void StartMotorDataTask(void *argument);
+extern void StartVolDataTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of VolQueue */
+  VolQueueHandle = osMessageQueueNew (16, sizeof(uint16_t), &VolQueue_attributes);
+
+  /* creation of MotorQueue */
+  MotorQueueHandle = osMessageQueueNew (16, sizeof(MotorMessage*), &MotorQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of VolSignTask */
+  VolSignTaskHandle = osThreadNew(StartVolSignTask, NULL, &VolSignTask_attributes);
+
+  /* creation of MotorCtrlTask */
+  MotorCtrlTaskHandle = osThreadNew(startMotorCtrlTask, NULL, &MotorCtrlTask_attributes);
+
+  /* creation of MotorDataTask */
+  MotorDataTaskHandle = osThreadNew(StartMotorDataTask, NULL, &MotorDataTask_attributes);
+
+  /* creation of VolDataTask */
+  VolDataTaskHandle = osThreadNew(StartVolDataTask, NULL, &VolDataTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartVolSignTask */
+/**
+  * @brief  Function implementing the VolSignTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartVolSignTask */
+__weak void StartVolSignTask(void *argument)
+{
+  /* USER CODE BEGIN StartVolSignTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartVolSignTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
